808ce1df68123cdbb45e9469bbbaf960eabe8331
Agustin Gonzalez <agustin.gonzalez.10@hotmail.com>
2025-07-29 10:35:24 -0300

    Clear cart when the user logs out

--- classes/Auth.php
+++ classes/Auth.php
@@ -48,6 +48,10 @@ class Auth {
         if (isset($_SESSION['user'])) {
             unset($_SESSION['user']);
         };
+
+        if (isset($_SESSION['cart'])) {
+            unset($_SESSION['cart']);
+        }
     }
 
     /**
